c60769b0e5480f14b28295e99a389038ec90cd64
FULVIO N U LEITE nicholas <fulnic@hotmail.com>
2022-08-01 08:40:42 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,7 @@
+    document.getElementById("azul").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "azul");
+    })
+
+    document.getElementById("roxo").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "roxo");
+    })
